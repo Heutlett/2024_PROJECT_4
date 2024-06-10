acfe0181c539b77142b42e97d95d62901e002de7
Justin Fernandez Badilla <jusfb18@gmail.com>
2024-06-10 00:42:32 -0600

    Metodos obtener reserva

---
+++ microservices/obtener_reserva_puntual/run.sh
@@ -0,0 +1,14 @@
+# Crear docker image en Dockerhub
+eval $(minikube docker-env)
+docker build -t obtener-reserva-puntual:latest .
+docker tag obtener-reserva-puntual:latest jusfb18/obtener-reserva-puntual:latest
+docker login
+docker push jusfb18/obtener-reserva-puntual:latest
+
+# Borrar el POD anterior
+kubectl delete deployment obtener-reserva-puntual
+kubectl delete service obtener-reserva-puntual-service
+
+# Subir el POD nuevo
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
