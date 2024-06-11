94a9e4466590a3c5b537a388424ec6b8349fefde
Justin Fernandez Badilla <jusfb18@gmail.com>
2024-06-10 23:01:31 -0600

    Broker estructura
    Co-authored-by: Jimena León <menaleon@users.noreply.github.com>

--- broker/broker.py
+++ broker/broker.py
@@ -4,48 +4,27 @@ import json
 
 app = Flask(__name__)
 
-base_url = "http://192.168.49.2:"
 
-metodos = {}
+baseurl = "http://192.168.49.2:"
 
-# GET
-
-metodos["obtener-reserva-puntual"] = {
-    "url":f"{base_url}30016/obtener-reserva-puntual",
-    "method":"GET"
+services = {
+    "cambiar-contrasena": f"{baseurl}30007/cambiar-contrasena", # funciona
+    "crear-reserva": f"{baseurl}30010/crear-reserva", # funciona
+    "crear-usuario": f"{baseurl}30004/crear-usuario", # funciona
+    "editar-reserva": f"{baseurl}30012/editar-reserva", # funciona
+    "eliminar-reserva":f"{baseurl}30013/eliminar-reserva", # CORREGIR QUE SOLO PARA TOKEN DE ADMIN
+    "obtener-reserva-puntual": f"{baseurl}30016/obtener-reserva/", # funciona
+    "obtener-reservas-futuras": f"{baseurl}30017/obtener-reserva/", # funciona
+    "obtener-reservas-pasadas": f"{baseurl}30015/obtener-reserva/", # REDEPLOY
+    "obtener-reservas-todas": f"{baseurl}30014/obtener-reserva/", # REDEPLOY
+    "obtener-usuario":f"{baseurl}30005/obtener-usuario", # funciona
+    "obtener-calendario": f"{baseurl}puerto/obtener-calendario",
 }
-metodos["obtener-usuario"] = {
-    "url": f"{base_url}30005/",
-    "method": "GET"
-}
-
-
-def hacer_metodo(metodo, data):
-    if metodo == "GET":
-        response = requests.get(metodos[data["method"]]["url"])
-    elif metodo == "POST":
-        response = requests.post(metodos[data["method"]]["url"], json=data)
-    elif metodo == "PUT":
-        response = requests.put(metodos[data["method"]]["url"], json=data)
-    elif metodo == "DELETE":
-        response = requests.delete(metodos[data["method"]]["url"])
-    if response.status_code == 200:
-        data = response.json()
-        return jsonify(data), 200
-    elif response.status_code != 500:
-        response_data = response.json()
-        return jsonify(response_data), response.status_code
-    else:
-        error_message = {'error': 'No se pudo obtener los datos de la API'}
-        return jsonify(error_message), 500
-
 
 # entry point de la cloud function
-@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
-def broker():
+@app.route('/<service_name>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+def broker(service_name):
     if request.method == "OPTIONS":
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
         headers = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
@@ -53,111 +32,73 @@ def broker():
             "Access-Control-Max-Age": "3600",
         }
         return ("", 204, headers)
-
+    
     headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": "true",
+        "Content-Type": "application/json"
     }
+    
 
-    print("Request: ", request)
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-    path = request.path
-    respuesta = {}
-    print(f"Path: {path}, Method: {request.method}, Request: {request_json}, Args: {request_args}, Headers: {request.headers}")
+    if service_name in services:
+        service_url = services[service_name]
 
-    if request.method == "GET":
-        try:
-            if path == "/":
-                try:
-                    username = request_args.get("username")
-                    password = request_args.get("password")
-                except Exception as e:
-                    respuesta = {
-                        "data": "",
-                        "status": 400,
-                        "message": "Error: Parametro 'username' o 'password' no encontrado."
-                    }
-                    return jsonify(respuesta), 400, headers
-                resultado,status_code = hacer_metodo(metodos["obtener-usuario"]["method"], f"{metodos['obtener-usuario']['url']}?username={username}&password={password}")
-                return jsonify(resultado), status_code, headers
-            else:
-                respuesta = {
-                    "data": "",
-                    "status": 400,
-                    "message": "Bad Request. Método no válido"
-                }
-                return jsonify(respuesta), 400, headers
-        except Exception as e:
-            respuesta = {
-                "data": "",
-                "status": 400,
-                "message": "Error: " + str(e)
-            }
-            return jsonify(respuesta), 400, headers
-    elif request.method == "POST":
-        try:
-            if path == "/broker" and request_json["data"]["method"] == "restablecer-password":
-                return jsonify({"response": "Contraseña restablecida"}), 200, headers
-            elif path == "/broker" and request_json["data"]["method"] == "crear-usuario":
-                return jsonify({"response": "Usuario creado"}), 200, headers
-            else:
-                respuesta = {
-                    "data": "",
-                    "status": 400,
-                    "message": "Bad Request. Método no válido"
-                }
-                return jsonify(respuesta), 400, headers
-        except Exception as e:
-            respuesta = {
-                "data": "",
-                "status": 400,
-                "message": "Error: " + str(e)
-            }
-            return jsonify(respuesta), 400, headers
-    elif request.method == "PUT":
-        try:
-            if path == "/broker" and request_json["data"]["method"] == "actualizar-usuario":
-                return jsonify({"response": "Usuario actualizado"}), 200, headers
-            else:
-                respuesta = {
-                    "data": "",
-                    "status": 400,
-                    "message": "Bad Request. Método no válido"
-                }
-                return jsonify(respuesta), 400, headers
-        except Exception as e:
-            respuesta = {
-                "data": "",
-                "status": 400,
-                "message": "Error: " + str(e)
-            }
-            return jsonify(respuesta), 400, headers
-    elif request.method == "DELETE":
-        try:
-            if path == "/broker" and request_args.get("method") == "eliminar-usuario":
-                return jsonify({"response": "Usuario eliminado"}), 200, headers
-            else:
-                respuesta = {
-                    "data": "",
-                    "status": 400,
-                    "message": "Bad Request. Método no válido"
-                }
-                return jsonify(respuesta), 400, headers
-        except Exception as e:
-            respuesta = {
-                "data": "",
-                "status": 400,
-                "message": "Error: " + str(e)
-            }
-            return jsonify(respuesta), 400, headers
+        if request.method == 'GET':
+            args = request.args if request.args else {}
+            
+            # Realizar solicitud GET con parámetros
+            if service_name=="obtener-usuario":
+                username = args.get('username')
+                password = args.get('password')
+                service_url += f"?username={username}&password={password}"
+            
+            if service_name == "obtener-reserva-puntual":
+                reservation_id = args.get('reservation_id')
+                service_url += f"?reservation_id={reservation_id}"
+
+            if service_name == "obtener-reservas-futuras":
+                time = args.get('time')
+                token = args.get('token')
+                service_url += f"?time={time}&token={token}"
+            
+            if service_name == "obtener-reservas-pasadas": # CORREGIR USER LOGICA
+                time = args.get('time')
+                token = args.get('token')
+                service_url += f"?time={time}&token={token}"
+            
+            if service_name == "obtener-reservas-todas": # CORREGIR LOGICA
+                time = args.get('time')
+                service_url += f"?time={time}"
+            
+            
+            response = requests.get(service_url)
+
+        elif request.method == 'POST':
+            try:
+                body = request.json if request.json else {}
+                response = requests.post(service_url, json=body, headers=headers)
+            except:
+                return jsonify("Error con los datos"),400,headers
+        elif request.method == 'PUT':
+            try:
+                body = request.json if request.json else {}
+                response = requests.put(service_url, json=body, headers=headers)
+            except:
+                return jsonify("Error con los datos"),400,headers
+            
+        elif request.method == 'DELETE':
+            args = request.args if request.args else {}
+            
+            # Realizar solicitud DELETE con parámetros
+            if service_name=="eliminar-reserva": 
+                reservation_id = args.get('reservation_id')
+                password = args.get('password')
+        
+        return response.content, response.status_code,headers
     else:
-        respuesta = {
-            "data": "",
-            "status": 400,
-            "message": "Bad Request. Método no válido"
-        }
-        return jsonify(respuesta), 400, headers
+        # Si el servicio no es encontrado, retornar error 404
+        return 'Service not found', 404
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5016, debug=True)
