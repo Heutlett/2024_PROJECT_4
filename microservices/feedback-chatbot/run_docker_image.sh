e80e1d157e925bc2ebd6506bb3d107762eb903bc
Justin Fernandez Badilla <jusfb18@gmail.com>
2024-06-11 02:31:38 -0600

    Actualizacion

--- microservices/feedback-chatbot/run_docker_image.sh
+++
@@ -1,5 +0,0 @@
-eval $(minikube docker-env)
-docker build -t feedback-chatbot:latest .
-docker tag obtener-menu:latest jusfb18/obtener-menu:latest
-docker login
-docker push jusfb18/obtener-menu:latest
