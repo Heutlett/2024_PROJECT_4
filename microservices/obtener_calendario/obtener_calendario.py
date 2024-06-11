910c7f864ab62a32fc6e15c45a1c18718609aa7d
Jimena <mena.leon@estudiantec.cr>
2024-06-10 18:19:54 -0600

    Obtener calendario

--- microservices/obtener_calendario/obtener_calendario.py
+++ microservices/obtener_calendario/obtener_calendario.py
@@ -42,71 +42,92 @@ def usar_bd_con_return(query):
         print(f"Error al ejecutar la consulta: {e}")
         return None
 
-    
-def sumar_hora(start_time_request):
-    decena = start_time_request[0]
-    unidad = start_time_request[1]
-    end_time = ""
-    # sumar 2 horas a la hora de inicio para obtener la hora de fin
-    if decena == "0":  
-        suma = int(unidad) + 2
-
-        if suma < 10:
-            end_time = "0" + str(suma) + start_time_request[2:]
-        else:
-            end_time = str(suma) + start_time_request[2:]
-
-    elif decena == "1":
-        suma = int(decena)*10 + int(unidad) + 2
-        end_time = str(suma) + start_time_request[2:]
-    
-    elif decena == "2":
-        suma = int(decena)*20 + int(unidad) + 2
-
-        if suma < 25:
-            end_time = str(suma) + start_time_request[2:]
-        else: # si la suma es mayor a 24
-            end_time = "00" + start_time_request[2:]
-    
-    return end_time
-
-# Funcion que compara dos horas en formato "HH:MM:SS"
-def hora1_menor_hora2(hora1, hora2):
-    if hora1 < hora2:
-        return True
-    else:
-        return False
-
-# Secret key to validate token
-secret_key="6af00dfe63f6495195a3341ef6406c2c"
-
-def obtener_reserva_puntual(reservation_id, headers):
-    result = usar_bd_con_return(f"SELECT * FROM Reservations WHERE Reservation_ID = '{reservation_id}';")
+def obtener_calendario_callback(date_request, start_time_request, restaurant_id, headers):
+    # json de respuesta
     mensaje = {}
-    mensaje["data"] = []
-    
-    if result == []:
-        mensaje["status"] = 400
-        mensaje["message"] = "No existe la reserva con el ID proporcionado."
-        return jsonify(mensaje), 400, headers
-    
-    for elem in result:
-        mensaje["data"].append({
-            "Reservation_ID": elem[0],
-            "Username": elem[1],
-            "Restaurant_ID": elem[2],
-            "Number_Of_People": elem[3],
-            "Date_Reserved": elem[4].strftime('%Y-%m-%d'),  # Convertir a cadena de texto en formato 'YYYY-MM-DD'
-            "Start_Time": elem[5].strftime('%H:%M:%S'),    # Convertir a cadena de texto en formato 'HH:MM:SS'
-            "End_Time": elem[6].strftime('%H:%M:%S')       # Convertir a cadena de texto en formato 'HH:MM:SS'
-        })
-    mensaje["status"] = 200
-    mensaje["message"] = "Reserva encontrada"
-    return jsonify(mensaje), 200, headers
+    mensaje['data'] = {}
+    mensaje['data']['available_tables'] = []
+
+    # Obtener todas las mesas disponibles para una fecha y hora especifica
+
+    try:
+        print("Date request: " + date_request)
+        print("Start time request: " + start_time_request)
+
+        # obtener todas las mesas
+        mesas = usar_bd_con_return(f"SELECT * FROM Tables WHERE Restaurant_ID = '{restaurant_id}'")
+        #return str(mesas), 200, headers             
+
+        # obtener las mesas ocupadas para esa fecha y hora
+        decena = start_time_request[0]
+        unidad = start_time_request[1]
+        end_time = ""
+        # sumar 2 horas a la hora de inicio para obtener la hora de fin
+        if decena == "0":  
+            suma = int(unidad) + 2
+
+            if suma < 10:
+                end_time = "0" + str(suma) + start_time_request[2:]
+            else:
+                end_time = str(suma) + start_time_request[2:]
+
+        elif decena == "1":
+            suma = int(decena)*10 + int(unidad) + 2
+            end_time = str(suma) + start_time_request[2:]
+        
+        elif decena == "2":
+            suma = int(decena)*20 + int(unidad) + 2
+
+            if suma < 25:
+                end_time = str(suma) + start_time_request[2:]
+            else: # si la suma es mayor a 24
+                end_time = "00" + start_time_request[2:]
+
+        print("End time  " + end_time)
+
+        mesas_ocupadas = usar_bd_con_return(f"SELECT * FROM Table_Availability WHERE Date_Reserved = '{date_request}' AND Start_Time >= '{start_time_request}' AND End_Time <= '{end_time}'")
+        
+        # cuando todas las mesas estan ocupadas, no hay mesas disponibles
+        # esto puede pasar en eventos especiales
+        if len(mesas_ocupadas) == len(mesas):
+            print("No hay mesas disponibles para la fecha y hora solicitada.")
+            mensaje['data']['available_tables'] = []
+            mensaje['status'] = 404
+            mensaje['message'] = "No hay mesas disponibles para la fecha y hora solicitada."
+            return jsonify(mensaje), 404, headers
+
+        # cuando no hay mesas ocupadas
+        if len(mesas_ocupadas) == 0:
+            for mesa in mesas:
+                mensaje['data']['available_tables'].append({"Table_ID": mesa[0], "Chairs" : mesa[1]})
+        else:
+            print("Hay algunas mesas ocupadas para la fecha y hora solicitada")
+            # cuando si hay mesas ocupadas
+            for mesa in mesas:
+                busy_table = True
+                for mesa_ocupada in mesas_ocupadas:
+                    if mesa[0] == mesa_ocupada[0]:
+                        busy_table = False
+                        break
+
+                if busy_table:
+                    mensaje['data']['available_tables'].append({"Table_ID": mesa[0], "Chairs" : mesa[1]})
+
+ 
+        mensaje['status'] = 200
+        mensaje['message'] = "Mesas disponibles obtenidas correctamente."
+        
+        
+        return jsonify(mensaje), 200, headers
+    except Exception as e:
+        mensaje['status'] = 500
+        mensaje['message'] = f"Error al obtener el calendario: {str(e)}"
+        return jsonify(mensaje), 500, headers
+
 
 # Obtiene una reserva puntual
-@app.route("/obtener-reserva/", methods=["GET", "OPTIONS"])
-def obtener_reservas():
+@app.route("/obtener-calendario", methods=["GET", "OPTIONS"])
+def obtener_calendario():
     if request.method == "OPTIONS":
         # Allows GET requests from any origin with the Content-Type
         # header and caches preflight response for an 3600s
@@ -127,14 +148,16 @@ def obtener_reservas():
     path = request.path
     respuesta = {}
 
-    if path == "/obtener-reserva/" and request.method == 'GET':
+    if path == "/obtener-calendario" and request.method == 'GET':
         try:
-            reservation_id = request_args.get("reservation_id")
+            date = request_args.get("date")
+            start_time = request_args.get("start_time")
+            restaurant_id = request_args.get("restaurant_id")
         except Exception as e:
             respuesta["message"] = "Error: Parametro 'reservation_id' no encontrado."
             return jsonify(respuesta), 400, headers
     
-        return obtener_reserva_puntual(reservation_id, headers)
+        return obtener_calendario_callback(date, start_time, restaurant_id, headers)
     else:
         respuesta["message"] = "Error: Metodo no valido."
         return jsonify(respuesta), 400, headers
