130258d3a76a0b8900502ab85e016215fb62c186
FALCH2000 <faleiva.ch@gmail.com>
2024-06-11 06:09:11 -0600

    Changes run sh services

--- microservices/cambiar_contrasena/run.sh
+++ microservices/cambiar_contrasena/run.sh
@@ -1,9 +1,9 @@
 # Crear imagen en Dockerhub
 eval $(minikube docker-env)
-docker build -t cambiar-contrasena:latest .
-docker tag cambiar-contrasena:latest jusfb18/cambiar-contrasena:latest
-docker login
-docker push jusfb18/cambiar-contrasena:latest
+#docker build -t cambiar-contrasena:latest .
+#docker tag cambiar-contrasena:latest jusfb18/cambiar-contrasena:latest
+#docker login
+#docker push jusfb18/cambiar-contrasena:latest
 
 # Borrar el POD anterior
 kubectl delete deployment cambiar-contrasena
--- microservices/editar_reserva/run.sh
+++ microservices/editar_reserva/run.sh
@@ -1,9 +1,9 @@
 # Crear docker image en Dockerhub
 eval $(minikube docker-env)
-docker build -t editar-reserva:latest .
-docker tag editar-reserva:latest jusfb18/editar-reserva:latest
-docker login
-docker push jusfb18/editar-reserva:latest
+#docker build -t editar-reserva:latest .
+#docker tag editar-reserva:latest jusfb18/editar-reserva:latest
+#docker login
+#docker push jusfb18/editar-reserva:latest
 
 # Borrar el POD anterior
 kubectl delete deployment editar-reserva
